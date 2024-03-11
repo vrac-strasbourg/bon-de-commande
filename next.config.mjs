8a7c3693ac602c7265c0363781cd35137a56b152
Thomas Guillet <thomas@palya.fr>
2024-03-11 23:01:23 +0100

    Corrige assetPrefix en production

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isProduction = 'production' === process.env.NODE_ENV
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProduction ? '/tracer' : '',
+    assetPrefix: isProduction ? '/bon-de-commande' : '',
     output: 'export',
     images: { unoptimized: true }
 };
