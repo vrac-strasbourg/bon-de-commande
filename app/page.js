0ee18fb2a63245e5c8fbf57980a97600586cad0e
Thomas Guillet <thomas@palya.fr>
2024-04-23 22:02:56 +0200

    Mutualise la logique de traduction et fallback au français

--- app/page.js
+++ app/page.js
@@ -6,20 +6,20 @@ import styles from "./page.module.css";
 import Qualite, { QualiteImage, map as QualiteMap } from "./qualite.js";
 import logo from "../public/logo.jpg";
 
-function bloc(item, field, texts) {
+function bloc(item, tp, t) {
   const ft = new Intl.NumberFormat("fr-FR", {
     style: "currency",
     currency: "EUR",
   });
   return (
     <tr key={item.id}>
-      <td>{item.fields[field]}</td>
+      <td>{tp(item)}</td>
       {Qualite({ qualites: item.fields.Qualite || [] })}
       <td className="number">{ft.format(item.fields.prix_TTC * 1.1)}</td>
       <td className="number">{ft.format(item.fields.prix_TTC * 0.9)}</td>
       <td className="number">{ft.format(item.fields.prix_TTC * 0.5)}</td>
       <td></td>
-      <td>{texts?.[item.fields.unit]?.[field]}</td>
+      <td>{t(item.fields.unit)}</td>
       <td></td>
     </tr>
   );
@@ -29,7 +29,18 @@ export default function Home() {
   const [products, setProducts] = useState([]);
   const [texts, setTexts] = useState([]);
   const [config, setConfig] = useState();
-  const [field, setField] = useState("Francais");
+  const fallback = "Francais";
+  const [field, setField] = useState(fallback);
+
+  const tp = (product) => {
+    const v = product.fields[field];
+    return v?.length ? v : product.fields[fallback];
+  };
+
+  const t = (prop) => {
+    const v = texts?.[prop]?.[field];
+    return v?.length ? v : texts?.[prop]?.[fallback];
+  };
 
   useEffect(() => {
     fetch(
@@ -63,16 +74,16 @@ export default function Home() {
       <header>
         <div>
           <h1>
-            {texts?.bc?.[field]} {texts?.[config?.Mois]?.[field]}
+            {t("bc")} {t(config?.Mois)}
           </h1>
 
           <ul>
-            <li>{texts?.nom?.[field]} :</li>
-            <li>{texts?.prénom?.[field]} :</li>
+            <li>{t("nom")} :</li>
+            <li>{t("prénom")} :</li>
             <li>
-              {texts?.email?.[field]} / {texts?.téléphone?.[field]} :
+              {t("email")} / {t("téléphone")} :
             </li>
-            <li>{texts?.groupement?.[field]} :</li>
+            <li>{t("groupement")} :</li>
           </ul>
         </div>
         <Image src={logo} height={100} priority alt="Logo de Vrac Strasbourg" />
@@ -84,46 +95,45 @@ export default function Home() {
       <table>
         <thead>
           <tr>
-            <th>{texts?.produit?.[field]}</th>
-            <th>{texts?.qualité?.[field]}</th>
-            <th>{`${texts?.tarif?.[field]} ${texts?.bleu?.[field]} (+10%)`}</th>
-            <th>{`${texts?.tarif?.[field]} ${texts?.vert?.[field]} (-10%)`}</th>
-            <th>{`${texts?.tarif?.[field]} ${texts?.rose?.[field]} (-50%)`}</th>
-            <th>{texts?.quantité?.[field]}</th>
-            <th>{texts?.unité?.[field]}</th>
-            <th>{texts?.montant?.[field]}</th>
+            <th>{t("produit")}</th>
+            <th>{t("qualité")}</th>
+            <th>{`${t("tarif")} ${t("bleu")} (+10%)`}</th>
+            <th>{`${t("tarif")} ${t("vert")} (-10%)`}</th>
+            <th>{`${t("tarif")} ${t("rose")} (-50%)`}</th>
+            <th>{t("quantité")}</th>
+            <th>{t("unité")}</th>
+            <th>{t("montant")}</th>
           </tr>
         </thead>
-        <tbody>{products.map((i) => bloc(i, field, texts))}</tbody>
+        <tbody>{products.map((i) => bloc(i, tp, t))}</tbody>
       </table>
       <footer>
         <div className="qualites">
           {Object.keys(QualiteMap).map((qualite) => (
             <div key={qualite}>
               {QualiteImage(qualite)}
-              {texts?.[qualite]?.[field]}
+              {t(qualite)}
             </div>
           ))}
         </div>
-        <p>{texts?.explications?.[field]}</p>
-        <h1>{texts?.tarifs?.[field]}</h1>
-        <h2>{texts?.contact?.[field]}</h2>
+        <p>{t("explications")}</p>
+        <h1>{t("tarifs")}</h1>
+        <h2>{t("contact")}</h2>
         <div className="contacts">
           <div>
-            {texts?.antenne?.[field]} Port du Rhin, Neuhof, Koenigshoffen,
+            {t("antenne")} Port du Rhin, Neuhof, Koenigshoffen,
             PARENCHchantement : 07 81 62 94 49 zoe‑strasbourg@vrac‑asso.org
           </div>
           <div>
-            {texts?.antenne?.[field]} Illkirch Ampère-Musau, Montagne Verte :
+            {t("antenne")} Illkirch Ampère-Musau, Montagne Verte :
             07 86 92 82 81 lea‑strasbourg@vrac‑asso.org
           </div>
           <div>
-            {texts?.antenne?.[field]} Hautepierre, Cité Spach :
+            {t("antenne")} Hautepierre, Cité Spach :
             07 66 67 95 66 alice‑strasbourg@vrac‑asso.org
           </div>
           <div>
-            {texts?.antenne?.[field]} Recyclerie by AMITEL (
-            {texts?.étudiants?.[field]}) :
+            {t("antenne")} Recyclerie by AMITEL ({t("étudiants")}) :
             07 68 57 13 33 julianne‑strasbourg@vrac‑asso.org
           </div>
         </div>
